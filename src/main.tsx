e80f5ce42943525a832834d7833f0fb7219cdb5e
aiyoubatiao <aiyoubatiao@users.noreply.github.com>
2025-11-18 01:26:50 +0800

    fix: 开发环境使用BrowserRouter，生产使用HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import { ConfigProvider } from 'antd'
 import zhCN from 'antd/locale/zh_CN'
 import App from './App.tsx'
 import './index.css'
 
+const RouterComp = import.meta.env.DEV ? BrowserRouter : HashRouter
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ConfigProvider locale={zhCN}>
-      <HashRouter>
+      <RouterComp>
         <App />
-      </HashRouter>
+      </RouterComp>
     </ConfigProvider>
   </React.StrictMode>,
 )
