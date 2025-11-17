1e5b4cc38b30d5d3c55b974086852d3b0c36d8d4
aiyoubatiao <aiyoubatiao@users.noreply.github.com>
2025-11-18 02:50:19 +0800

    feat: 更新功能与构建配置

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { ConfigProvider } from 'antd'
 import zhCN from 'antd/locale/zh_CN'
 import App from './App.tsx'
 import './index.css'
 
-const RouterComp = import.meta.env.DEV ? BrowserRouter : HashRouter
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ConfigProvider locale={zhCN}>
-      <RouterComp>
+      <BrowserRouter>
         <App />
-      </RouterComp>
+      </BrowserRouter>
     </ConfigProvider>
   </React.StrictMode>,
 )
